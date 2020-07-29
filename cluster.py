2a1fc69ba15414e5526027ca13e99a80d98fb272
harkib <harkib@sfu.ca>
2020-07-29 14:37:32 -0700

    Using weekly and daily for testing

--- cluster.py
+++ cluster.py
@@ -7,11 +7,18 @@ import pickle
 class Clusters:
 
     clusters =  {}
-    correlations = {}
     n_clusters = 0
-    # n_stocks = 0
 
-    info_n_stocks = 0
+    correlations_daily = {}
+    correlations_weekly = {}
+
+    n_clusters_occupied_daily = 0
+    n_clusters_occupied_weekly = 0
+
+    r_daily = 0
+    r_weekly = 0
+    r_avg = 0
+ 
     
 
     def __init__(self,n_clusters_, Y, Y_name):
@@ -22,30 +29,48 @@ class Clusters:
         for y, stock in zip(Y,Y_name):
             self.clusters[y].append(stock)
     
-    def correlation(self, test_data):
+    def correlation(self, test_daily, test_weekly):
 
-        # self.n_stocks = len(test_data['Stock'])
-        test_data = test_data.set_index('Stock', drop =True).transpose()
-    
+        test_daily = test_daily.set_index('Stock', drop =True).transpose()
+        test_weekly = test_weekly.set_index('Stock', drop =True).transpose()
+
+        # compute daily r
         r_sum = 0
         for key in self.clusters.keys():
             
             stocks = self.clusters[key]
-            self.info_n_stocks += len(stocks)
-
             if len(stocks) > 1:
-                rs = np.array(test_data[stocks].corr())[np.triu_indices(len(stocks),k=1)]
+                rs = np.array(test_daily[stocks].corr())[np.triu_indices(len(stocks),k=1)]
                 r_avg = np.average(rs)
                 r_sum += r_avg
-                self.correlations[key] = r_avg
+                self.correlations_daily[key] = r_avg
+                self.n_clusters_occupied_daily += 1
 
-        return r_sum/self.n_clusters
+        r_daily = r_sum/self.n_clusters_occupied_daily
+
+        # compute weekly r
+        r_sum = 0
+        for key in self.clusters.keys():
+            
+            stocks = self.clusters[key]
+            if len(stocks) > 1:
+                rs = np.array(test_weekly[stocks].corr())[np.triu_indices(len(stocks),k=1)]
+                r_avg = np.average(rs)
+                r_sum += r_avg
+                self.correlations_weekly[key] = r_avg
+                self.n_clusters_occupied_weekly += 1
+
+        r_weekly = r_sum/self.n_clusters_occupied_weekly
+
+        r_avg = (r_daily + r_weekly)/2
+        
+        return {'Avg': r_avg, 'Daily': r_daily, 'Weekly':r_weekly}
 
     def print_(self,n=10):
 
         for key in self.clusters.keys():
             if len(self.clusters[key]) > 1:
-                print(key, self.clusters[key][:n],self.correlations[key] )
+                print(key, self.clusters[key][:n],'Corr:',self.correlations_daily[key] )
 
 
     
@@ -59,7 +84,8 @@ if __name__ == '__main__':
     weekly = pd.read_pickle('Data\weekly.pkl')
     monthly = pd.read_pickle('Data\monthly.pkl')
     GICS = pd.read_csv('Data\GICS-wiki.csv',encoding='ANSI')
-    test = pd.read_pickle('Data\\test.pkl')
+    test_daily = pd.read_pickle(r'Data\test_daily.pkl')
+    test_weekly = pd.read_pickle(r'Data\test_weekly.pkl')
 
     # define input
     x_cols = daily.columns[:-2]
@@ -68,12 +94,12 @@ if __name__ == '__main__':
 
     # build model
     n_clusters = 100
-    model = KMeans(n_clusters=n_clusters)
+    model = AgglomerativeClustering(n_clusters=n_clusters)
     Y = model.fit_predict(X)
 
     # evaluate model
     KMeans_Clusters = Clusters(n_clusters,Y,Y_stock)
-    print('Average Cluster Correlation:', KMeans_Clusters.correlation(test))
+    print('Cluster Correlation:', KMeans_Clusters.correlation(test_daily, test_weekly))
     KMeans_Clusters.print_()
 
 
